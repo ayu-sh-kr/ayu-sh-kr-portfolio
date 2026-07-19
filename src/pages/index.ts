a25ba54011375cd29fc8b141fd31cb24f6a4ae03
Ayush Jaiswal <akjaiswal2003@gmail.com>
2026-07-19 15:54:18 +0530

    Export blog pages from pages index
    Add `BlogPage` and `BlogSlugPage` to `src/pages/index.ts` so they can be imported from the pages barrel alongside `HomePage` and `ErrorPage`.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 export { HomePage } from "@app/pages/home.page.ts";
 export { ErrorPage } from "@app/pages/error.page.ts";
+export { BlogPage } from "@app/pages/blog.page.ts";
+export { BlogSlugPage } from "@app/pages/blog-slug.page.ts";
