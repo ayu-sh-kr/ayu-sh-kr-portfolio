838a0d79378e2cfa24f3701d77c5e0a761004b62
Ayush Jaiswal <akjaiswal2003@gmail.com>
2026-07-23 19:05:21 +0530

    Export PricingPage from pages index
    Add `PricingPage` to `src/pages/index.ts` so it is available through the shared pages barrel export, aligning it with the other page exports.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { HomePage } from "@app/pages/home.page.ts";
 export { ErrorPage } from "@app/pages/error.page.ts";
 export { BlogPage } from "@app/pages/blog.page.ts";
 export { BlogSlugPage } from "@app/pages/blog-slug.page.ts";
+export { PricingPage } from "@app/pages/pricing.page.ts";
