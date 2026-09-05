5a447eb65cddc13d7dae6ed57f7ade3d4f96c2d2
Ayush Jaiswal <akjaiswal2003@gmail.com>
2026-09-05 16:41:41 +0530

    Update `pricing-estimator-type-options.component` button classes for consistency and improved form aesthetics

--- src/components/pages/pricing/estimator/pricing-estimator-type-options/pricing-estimator-type-options.component.ts
+++ src/components/pages/pricing/estimator/pricing-estimator-type-options/pricing-estimator-type-options.component.ts
@@ -105,7 +105,7 @@ export class PricingEstimatorTypeOptionsComponent extends BaseElement {
           ${content.types
             .map(
               (option) => `
-                <button class="pricing-pick ${option.id === this.selectedId ? "is-selected" : ""}" type="button" data-estimator-id="${option.id}" aria-pressed="${option.id === this.selectedId}">
+                <button class="form-choice input-lg input-rounded-md input-bordered pricing-pick ${option.id === this.selectedId ? "is-selected" : ""}" type="button" data-estimator-id="${option.id}" aria-pressed="${option.id === this.selectedId}">
                   <span class="pricing-pick-icon">${option.icon}</span>
                   <span class="pricing-pick-label">${option.label}</span>
                   <span class="pricing-pick-sublabel">${option.sublabel}</span>
